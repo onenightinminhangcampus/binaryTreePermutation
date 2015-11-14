7ff152df30a0b1fc9590e13c40c866e49c3896c7
onenightinminhangcampus <onenightinminhangcampus@gmail.com>
2015-11-15 02:27:24 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+	//int count = 0;
+	int n = 3;
+	
+	vector<vector <int>> permutation;
+	//n=3,i=	     0
+	permutation = BinaryTreePermutation(n);
+	
+cout << permutation.size() << endl;
+	return 0;
+	
+	}
